fad327735eeed85d81887db8ec02dbbc9ed91dbf
MrSentsov <84343346+MrSentsov@users.noreply.github.com>
2021-06-23 12:41:53 -0400

    Begginings of working on the multiplication function

--- cypress/integration/tests/multiplication.js
+++ cypress/integration/tests/multiplication.js
@@ -1,3 +1,6 @@
+const firstNumber = "cy.get('span#x4span').text()"
+const secondNumber = "cy.get('span#y4span').text()"
+const answer = "span#x4span * span#y4span"
 
 describe(
     "visits the website",
@@ -14,9 +17,17 @@ describe(
                     }
                 })
                 cy.get(':nth-child(1) > table > tbody > :nth-child(3) > :nth-child(2) > .M').click()
-                
+                cy.get('input[type="radio"]').get('input[value="4"]').check().should('be.checked')
+                cy.get('select[name="numqstn"]').select('20')
+                cy.get('input[type="image"]').click()
             }
 
+        )
+        it(
+            "Multiplies the two numbers",
+            ()=>{
+                cy.get('input#response').type(answer)
+            }
         )
     }
 )
