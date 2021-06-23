ba267f95a8efa01ea2ff646ee8cf46447083d212
MrSentsov <84343346+MrSentsov@users.noreply.github.com>
2021-06-23 12:54:24 -0400

    added an eslint plugin for recognizing cypress methods

--- cypress/integration/tests/multiplication.js
+++ cypress/integration/tests/multiplication.js
@@ -4,29 +4,36 @@ const answer = "span#x4span * span#y4span"
 
 describe(
     "visits the website",
-    ()=>{
+    () => {
         it(
             "visits the website",
-            ()=>{
-                cy.visit('https://lizardpoint.com/')
-                cy.get('.navbar-nav')
-                .find('a').each(($el, index, $list) =>{
-                    if($el.text() === 'Math')
-                    {
-                    cy.wrap($el).click()
-                    }
-                })
-                cy.get(':nth-child(1) > table > tbody > :nth-child(3) > :nth-child(2) > .M').click()
-                cy.get('input[type="radio"]').get('input[value="4"]').check().should('be.checked')
-                cy.get('select[name="numqstn"]').select('20')
-                cy.get('input[type="image"]').click()
+            () => {
+                cy.visit("https://lizardpoint.com/")
+                cy.get(".navbar-nav")
+                    .find("a")
+                    .each(($el, index, $list) => {
+                        if ($el.text() === "Math") {
+                            cy.wrap($el)
+                                .click()
+                        }
+                    })
+                cy.get(":nth-child(1) > table > tbody > :nth-child(3) > :nth-child(2) > .M")
+                    .click()
+                cy.get("input[type=\"radio\"]")
+                    .get("input[value=\"4\"]")
+                    .check()
+                    .should("be.checked")
+                cy.get("select[name=\"numqstn\"]")
+                    .select("20")
+                cy.get("input[type=\"image\"]")
+                    .click()
             }
 
         )
         it(
             "Multiplies the two numbers",
-            ()=>{
-                cy.get('input#response').type(answer)
+            () => {
+                cy.get("input#response").type(answer)
             }
         )
     }
